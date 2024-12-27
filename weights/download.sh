a4310d009d64ea2f87380b8684d71ea99cb10def
ahmedmoawad124 <ahmedmoawad124@gmail.com>
2024-12-27 20:31:55 +0200

    weights downloading

---
+++ weights/download.sh
@@ -0,0 +1,11 @@
+# vanilla_cnn
+mkdir -p ./weights/vanilla_cnn
+gdown https://drive.google.com/uc?id=1ecBr6XkgTpBrhEuaJMjMPlqy5bZ4yQun -O ./weights/vanilla_cnn/best_model.pth
+
+# resnet18
+mkdir -p ./weights/resnet18
+gdown https://drive.google.com/uc?id=1prt596Ua4gce9hkEcnCDv6dVnPs6sajd -O ./weights/resnet18/best_model.pth
+
+# mobilenet_v2
+mkdir -p ./weights/mobilenet_v2
+gdown https://drive.google.com/uc?id=1HyHFdDFRvIGTjIBMkzvIqe_zjtAT-AnZ -O ./weights/mobilenet_v2/best_model.pth
